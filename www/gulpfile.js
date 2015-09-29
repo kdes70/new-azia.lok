c685c1db49b82b00ceb7a8aac6936b663827ad14
Dmitry <kdes70@mail.ru>
2015-09-29 15:19:01 +0300

    init commit

---
+++ www/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    wiredep = require('wiredep').stream;
+
+
+gulp.task('bower', function () {
+    gulp.src('./index.html')
+        .pipe(wiredep({
+           directory: "bower_components"
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('bower.json', ['bower']);
+});
